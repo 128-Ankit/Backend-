6a402deaf3e87f7186984acd851a36396444608a
Ankit Kumar <abhishekankit128@gmail.com>
2024-03-07 15:48:53 +0530

    adding routes

--- 01 fist class/index.js
+++ 01 fist class/index.js
@@ -2,6 +2,19 @@
 const express = require("express");
 const app = express();
 
+//Routes
+//get request
+app.get("/", (req, res) => {
+    res.send("Wow! server is live now, and it's working!");
+});
+
+//post req
+app.post("/api/cars",(req,res) => {
+    const{name, brand} =req.body;
+    console.log(name);
+    console.log(brand);
+});
+
 //Active server on port
 app.listen(3000, ()=>{
     console.log("Server is live on port", 3000)
